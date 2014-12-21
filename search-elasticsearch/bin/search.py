33f05ea8491aaf20472d2e8464d62bf22d0f4fcf
eperry <edwardperry1@gmail.com>
2014-12-21 22:27:07 +0000

    Updated Event to _raw as splunk requires that as the default field

--- search-elasticsearch/bin/search.py
+++ search-elasticsearch/bin/search.py
@@ -8,14 +8,11 @@ pp = pprint.PrettyPrinter(indent=4)
 es = Elasticsearch()
 
 res = es.search(size=50, index="*", body={"query": {"match_all": {}}})
-print("\"_time\",\"Event\"")
-#pp (  res )
+print("\"_time\",\"_raw\"")
 for hit in res['hits']['hits']:
    hit["_source"]["message"] = hit["_source"]["message"].replace('"',' ');
    epochTimestamp = hit['_source']['@timestamp'];
    date_time = '2014-12-21T16:11:18.419Z'
    pattern = '%Y-%m-%dT%H:%M:%S.%fZ'
-   print ("Test")
    hit['_source']['_epoch'] = int(time.mktime(time.strptime(epochTimestamp, pattern)))
    print("%(_epoch)s,\"%(message)s\" " % hit["_source"] )
-   #pp.pprint(hit);
