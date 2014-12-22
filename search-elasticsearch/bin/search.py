f2a2c6592fe33d135e6ded86fed8c1b6180d6c92
splunk <splunk@rftnagp01.hbc.com>
2014-12-22 14:39:18 -0500

    Added dynamic default field option to allow the script to return different elastic search fields field=<elasticserach field name>

--- search-elasticsearch/bin/search.py
+++ search-elasticsearch/bin/search.py
@@ -8,12 +8,15 @@ import sys
 total = len(sys.argv)
 cmdargs = str(sys.argv)
 index="*";
+defaultField="message";
 for i in xrange(total):
    opt = str (sys.argv[i]);
    if (  opt.startswith("index") ):
       index= str (sys.argv[i]).split("=")[1];
    elif (  opt.startswith("query") ):
       query= str (sys.argv[i]).split("=")[1];
+   elif (  opt.startswith("field") ):
+      defaultField= str (sys.argv[i]).split("=")[1];
    
 pp = pprint.PrettyPrinter(indent=4)
 es = Elasticsearch()
@@ -21,11 +24,12 @@ res = es.search(size=50, index=index, body={"query": {"match_all": {}} })
 print("\"_time\",\"_raw\",\"index\",\"type\"")
 #pp.pprint(res);
 for hit in res['hits']['hits']:
-   hit["_source"]["message"] = hit["_source"]["message"].replace('"',' ');
+   hit["_source"][defaultField] = hit["_source"][defaultField].replace('"',' ');
    epochTimestamp = hit['_source']['@timestamp'];
    date_time = '2014-12-21T16:11:18.419Z'
    pattern = '%Y-%m-%dT%H:%M:%S.%fZ'
    hit['_source']['_epoch'] = int(time.mktime(time.strptime(epochTimestamp, pattern)))
-   print("%(_epoch)s,\"%(message)s\"," % hit["_source"] + 
+   hit['_source']["_raw"]=hit['_source'][defaultField]
+   print("%(_epoch)s,\"%(_raw)s\"," % hit["_source"] + 
          "\"%(_index)s\",\"%(_type)s\"" % hit
          )
